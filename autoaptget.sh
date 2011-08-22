d8573bb6e2b7717c4b09c0aa8b86da09836bfbc4
JensDiemer <git@jensdiemer.de>
2011-08-22 11:38:46 +0200

    Do the same with apt-get ;)

---
+++ autoaptget.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+function info {
+    echo ""
+    echo "**** $1 ****"
+    echo ""
+}
+function verbose_eval {
+    echo "--------------------------------------------------------------------"
+    echo $*
+    echo "--------------------------------------------------------------------"
+    eval $*
+}
+
+if [ $(whoami) != 'root' ]; then
+    info "Error: You must start this script with sudo!"
+    exit
+fi
+
+info "You should run 'apt-get update' before!"
+
+info "Mark all packages as 'Automatically installed'"
+verbose_eval apt-mark markauto `apt-mark showauto`
+
+info "Mark all packages from 'packagelist.txt' as 'manual'"
+verbose_eval apt-mark unmarkauto `cat packagelist.txt | grep -v '^#' | tr '\n' ' '`
+
+info "Install packages from 'packagelist.txt'"
+verbose_eval apt-get install `cat packagelist.txt | grep -v '^#' | tr '\n' ' '`
+
+info "Remove all unneeded packages"
+verbose_eval apt-get autoremove
